92c1ffc06f09071d03f68b2c08c8790c7fb2651d
Boris Burgstaller <boris.burgstaller@netcentric.biz>
2021-03-15 10:06:21 +0100

    improve rsync

--- deployAndRestart.sh
+++ deployAndRestart.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-rsync -avzh . pi@octopi.local:~/OctoRelay
+rsync -avzh --exclude='.git/' --exclude='.DS_Store' --exclude='deployAndRestart.sh' --delete . pi@octopi.local:~/OctoRelay
 ssh pi@octopi.local "~/OctoRelay/installOnPiAndRestart.sh"
