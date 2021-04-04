57a71a34fd50a586ba771eb0cfee1d6247daaa33
Robin Tail <robin_tail@me.com>
2021-04-04 12:32:43 +0200

    Confirmation dialog sample implementation including minor code style fixes of JS.

--- octoprint_octorelay/static/js/octorelay.js
+++ octoprint_octorelay/static/js/octorelay.js
@@ -5,34 +5,51 @@ $(function() {
         self.loginState = parameters[1];
 
         self.onDataUpdaterPluginMessage = function(plugin, data) {
-            if (plugin != "octorelay") {
+            if (plugin !== "octorelay") {
                 return;
             }
-            //console.log("onDataUpdaterPluginMessage: "+JSON.stringify(data));
+            // console.log("onDataUpdaterPluginMessage: " + JSON.stringify(data));
             for (const [key, value] of Object.entries(data)) {
-                //console.log(JSON.stringify(key));
-                //console.log(JSON.stringify(value));
+                // console.log(JSON.stringify(key));
+                // console.log(JSON.stringify(value));
                 if (value.active !== undefined) {
-                    if(value.active == 1) {
-                        $("#relais"+key).show();
+                    if (value.active === 1) {
+                        $("#relais" + key).show();
                     } else {
-                        $("#relais"+key).hide();
+                        $("#relais" + key).hide();
                     }
                 }
                 if (value.iconText !== undefined) {
-                    $("#ralayIcon"+key).html(value.iconText);
+                    $("#ralayIcon" + key).html(value.iconText);
                 }
                 if (value.labelText !== undefined) {
-                    $("#ralayIcon"+key).attr("title", (value.labelText));
+                    $("#ralayIcon" + key).attr("title", value.labelText);
                 }
                 if (value.confirmOff !== undefined) {
-                    $("#ralayIcon"+key).attr("data-confirm", (value.confirmOff));
+                    $("#ralayIcon" + key).attr("data-confirm", parseInt(value.confirmOff, 10));
                 }
             }
         };
     }
+
+    function OctoRelayConfirmClick(pin, title, confirmOff) {
+        // console.log("confirmOff " + confirmOff);
+        if (confirmOff === 0) {
+            return OctoPrint.simpleApiCommand("octorelay", "update", pin);
+        }
+        $("#octorelay-confirmation-dialog .modal-title").text("Turning " + title + " off");
+        $("#octorelay-confirmation-text").text("Are you sure you want to turn the " + title + " off?")
+        $("#octorelay-confirmation-dialog .btn-cancel").off("click").on("click", function() {
+            $("#octorelay-confirmation-dialog").modal("hide");
+        });
+        $("#octorelay-confirmation-dialog .btn-confirm").off("click").on("click", function() {
+            OctoPrint.simpleApiCommand("octorelay", "update", pin);
+        });
+        $("#octorelay-confirmation-dialog").modal("show");
+    }
+
     OCTOPRINT_VIEWMODELS.push({
         construct: OctoRelayViewModel,
-        dependencies: ["settingsViewModel","loginStateViewModel"],
+        dependencies: ["settingsViewModel", "loginStateViewModel"],
     });
 });
