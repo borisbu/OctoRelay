5349dffa2bccd44d659434ff9656c8a079d72a8f
Robin Tail <robin_tail@me.com>
2023-07-02 22:29:07 +0200

    Model props test.

--- ts/octorelay.spec.ts
+++ ts/octorelay.spec.ts
@@ -1,13 +1,24 @@
 describe("OctoRelayViewModel", () => {
+  const registry: ViewModel[] = [];
+  const jQueryMock = jest.fn((cb: () => void) => cb());
+
+  Object.assign(global, {
+    OCTOPRINT_VIEWMODELS: registry,
+    $: jQueryMock,
+  });
+  require("./octorelay");
+
   test("Should push the model into the registry", () => {
-    Object.assign(global, {
-      OCTOPRINT_VIEWMODELS: [],
-      $: jest.fn((cb: () => void) => cb()),
-    });
+    expect(jQueryMock).toHaveBeenCalled();
+    expect(registry).toHaveLength(1);
+    expect(registry[0]).toMatchSnapshot();
+  });
 
-    require("./octorelay");
-
-    expect($).toHaveBeenCalled();
-    expect(OCTOPRINT_VIEWMODELS).toHaveLength(1);
+  test("Constructor should set its certain properties", () => {
+    const [model] = registry;
+    const { construct } = model;
+    expect(Object.keys(construct)).toEqual([]);
+    construct.call(construct, [{ settings: true }, { login: true }]);
+    expect({ ...construct }).toMatchSnapshot();
   });
 });
