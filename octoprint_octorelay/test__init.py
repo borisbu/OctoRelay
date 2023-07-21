c778ecdde9314e921929864a93f6d1ea87467ef9
Robin Tail <robin_tail@me.com>
2023-07-21 23:41:20 +0200

    Mock RPi before importing the module.

--- octoprint_octorelay/test__init.py
+++ octoprint_octorelay/test__init.py
@@ -2,6 +2,11 @@ import unittest
 from unittest.mock import Mock, patch, MagicMock
 import os
 
+# Patch RPi.GPIO module before importing OctoRelayPlugin class
+RPi_mock = Mock()
+sys.modules['RPi'] = RPi_mock
+sys.modules['RPi.GPIO'] = RPi_mock
+
 from __init__ import OctoRelayPlugin
 
 class TestOctoRelayPlugin(unittest.TestCase):
