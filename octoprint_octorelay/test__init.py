eb62baa2d3107befca421f73cdf71109db794c7f
Robin Tail <robin_tail@me.com>
2023-07-21 23:53:58 +0200

    Instantiation and teardowning.

--- octoprint_octorelay/test__init.py
+++ octoprint_octorelay/test__init.py
@@ -11,7 +11,17 @@ sys.modules['RPi.GPIO'] = RPi_mock
 from __init__ import OctoRelayPlugin
 
 class TestOctoRelayPlugin(unittest.TestCase):
-    pass
+
+    @classmethod
+    def setUpClass(cls):
+        # Create an instance of the OctoRelayPlugin class
+        cls.plugin_instance = octorelay.OctoRelayPlugin()
+
+    @classmethod
+    def tearDownClass(cls):
+        # Clean up
+        del sys.modules['RPi']
+        del sys.modules['RPi.GPIO']
 
 if __name__ == '__main__':
     unittest.main()
