a23243722de7f9c0a6c4c75dab5b09519a5aba2f
Robin Tail <robin_tail@me.com>
2023-07-21 23:31:58 +0200

    Sample test.

---
+++ octoprint_octorelay/test__init.py
@@ -0,0 +1,12 @@
+import unittest
+from unittest.mock import Mock, patch
+import RPi.GPIO as GPIO
+import os
+
+from __init__ import OctoRelayPlugin
+
+class TestOctoRelayPlugin(unittest.TestCase):
+    pass
+
+if __name__ == '__main__':
+    unittest.main()
