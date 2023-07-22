7d8737c115807f83f3cb899ddb801195dc6dbc0e
Robin Tail <robin_tail@me.com>
2023-07-22 22:07:52 +0200

    Last branch: update command case.

--- octoprint_octorelay/test__init.py
+++ octoprint_octorelay/test__init.py
@@ -416,7 +416,8 @@ class TestOctoRelayPlugin(unittest.TestCase):
         self.plugin_instance.update_ui = originalUpdate
 
     @patch('flask.jsonify')
-    def test_on_api_command(self, json):
+    @patch('os.system')
+    def test_on_api_command(self, json, systemMock):
         originalUpdate = self.plugin_instance.update_ui
         self.plugin_instance.update_ui = Mock()
         GPIO_mock.input = Mock(return_value=True)
@@ -462,6 +463,14 @@ class TestOctoRelayPlugin(unittest.TestCase):
                 "data": { "pin": "r4" },
                 "inverted": False,
                 "expectedStatus": True
+            },
+            {
+                "command": "update",
+                "data": { "pin": "r4" },
+                "inverted": True,
+                "expectedStatus": "ok",
+                "expectedOutput": True,
+                "expectedCommand": "CommandOnMock"
             }
         ]
         for case in cases:
@@ -469,12 +478,18 @@ class TestOctoRelayPlugin(unittest.TestCase):
                 "active": True,
                 "relay_pin": 17,
                 "inverted_output": case["inverted"],
-                "labelText": "TEST"
+                "labelText": "TEST",
+                "cmdON": "CommandOnMock",
+                "cmdOFF": "CommandOffMock"
             }
             self.plugin_instance._settings.get = Mock(return_value=settingValueMock)
             self.plugin_instance.on_api_command(case["command"], case["data"])
             if hasattr(case, "expectedJson"):
                 json.assert_called_with(case["expectedJson"])
+            if hasattr(case, "expectedOutput"):
+                GPIO_mock.output.assert_called_with("r4", case["expectedOutput"])
+            if hasattr(case, "expectedCommand"):
+                systemMock.assert_called_with(case["expectedCommand"])
             if hasattr(case, "expectedStatus"):
                 json.assert_called_with(status=case["expectedStatus"])
 
