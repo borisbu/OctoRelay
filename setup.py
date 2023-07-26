cc52d636a3ad9ef34a66786a4e4fa382c769b286
Robin Tail <robin_tail@me.com>
2023-07-26 18:15:13 +0200

    Next version: 2.1.0rc2.

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ plugin_package = "octoprint_octorelay"
 plugin_name = "OctoRelay"
 
 # The plugin's version. Can be overwritten within OctoPrint's internal data via __plugin_version__ in the plugin module
-plugin_version = "2.1.0rc1"
+plugin_version = "2.1.0rc2"
 
 # The plugin's description. Can be overwritten within OctoPrint's internal data via __plugin_description__ in the plugin
 # module
