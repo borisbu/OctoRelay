6d905bdd51ea398ca615cd14c4e2a56789555db8
Anna Bocharova <robin_tail@me.com>
2023-07-28 21:51:51 +0000

    Using module name for importing version.

--- octoprint_octorelay/__init__.py
+++ octoprint_octorelay/__init__.py
@@ -312,4 +312,5 @@ __plugin_hooks__ = {
     "octoprint.comm.protocol.atcommand.sending":
         __plugin_implementation__.process_at_command
 }
-from ._version import __version__
+
+from octoprint_octorelay._version import __version__
