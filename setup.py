d6d3f1a29621659445152e8e8f2a209c4c9d92a1
Anna Bocharova <robin_tail@me.com>
2023-07-29 10:05:55 +0200

    minor, empty line in setup.py

--- setup.py
+++ setup.py
@@ -21,7 +21,6 @@ miniver_version, cmdclass = get_version_and_cmdclass(r"octoprint_octorelay")
 # Do not forget to adjust the following variables to your own plugin.
 
 # The plugin's identifier, has to be unique
-
 plugin_identifier = "octorelay"
 
 # The plugin's python package, should be "octoprint_<plugin identifier>", has to be unique
