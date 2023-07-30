ee7feb7839d6f2ff324e030113a8a2e78c9c5224
Robin Tail <robin_tail@me.com>
2023-07-30 21:02:11 +0200

    Making a stub for installations from sources.

---
+++ ts/stub/octorelay.ts
@@ -0,0 +1,53 @@
+/**
+ * WARNING: This is a stub for those who installs the plugin from sources
+ *
+ * PLEASE NOTE the new URL for installing the distributed package:
+ * @link https://github.com/borisbu/OctoRelay/releases/download/latest/release.zip
+ */
+
+$(() => {
+  const handleClick = () => {
+    const dialog = $("#octorelay-confirmation-dialog");
+    dialog.find(".modal-title").text("OctoRelay needs one more update");
+    dialog
+      .find("#octorelay-confirmation-text")
+      .text(
+        "We switched to distributing the plugin in the form of a specially prepared installation " +
+          "package instead of installing from source. The URL for updates has changed because of this. " +
+          'However, there is no cause for concern. Please go to "Software update" section of OctoPrint ' +
+          "settings and update OctoRelay one more time. We apologize for the inconvenience." +
+          "As a last resort, install manually using the following URL:" +
+          "https://github.com/borisbu/OctoRelay/releases/download/latest/release.zip"
+      );
+    dialog.find(".btn-cancel").hide();
+    dialog
+      .find(".btn-confirm")
+      .off("click")
+      .text("OK")
+      .on("click", () => {
+        dialog.modal("hide");
+      });
+    dialog.modal("show");
+  };
+  $("#relaisr1")
+    .css({
+      display: "flex",
+      float: "left",
+      width: "40px",
+      height: "40px",
+      padding: "unset",
+      cursor: "pointer",
+      color: "yellow",
+      "font-size": "1.25rem",
+      "text-decoration": "none",
+      "align-items": "center",
+      "justify-content": "center",
+      "background-color": "black",
+      "border-radius": "5px",
+    })
+    .show()
+    .html('<i class="fa fa-warning"></i>')
+    .attr("title", "Click to read the important notice from OctoRelay")
+    .off("click")
+    .on("click", handleClick);
+});
