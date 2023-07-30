6126eefee403d0aae1070173eafa3b5c4e7f2703
Robin Tail <robin_tail@me.com>
2023-07-30 21:08:24 +0200

    Minor: grammar.

--- octoprint_octorelay/static/js/octorelay.js
+++ octoprint_octorelay/static/js/octorelay.js
@@ -12,10 +12,11 @@ $(() => {
         dialog
             .find("#octorelay-confirmation-text")
             .text("We switched to distributing the plugin in the form of a specially prepared installation " +
-            "package instead of installing from source. The URL for updates has changed because of this. " +
-            'However, there is no cause for concern. Please go to "Software update" section of OctoPrint ' +
-            "settings and update OctoRelay one more time. We apologize for the inconvenience." +
-            "As a last resort, install manually using the following URL:" +
+            "package instead of offering the installation from sources. " +
+            "The URL for fetching updates has changed because of this. " +
+            'However, there is no cause for concern: please go to "Software update" section of the OctoPrint ' +
+            "settings and update OctoRelay one more time. We apologize for the inconvenience. " +
+            "As a last resort, you can always install it manually using the following URL: " +
             "https://github.com/borisbu/OctoRelay/releases/download/latest/release.zip");
         dialog.find(".btn-cancel").hide();
         dialog
