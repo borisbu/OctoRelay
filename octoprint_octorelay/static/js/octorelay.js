b7e15f5c747db997af75dfd1d0d8b9f250f5b899
Robin Tail <robin_tail@me.com>
2023-07-30 22:17:52 +0200

    Minor: text.

--- octoprint_octorelay/static/js/octorelay.js
+++ octoprint_octorelay/static/js/octorelay.js
@@ -21,8 +21,8 @@ $(() => {
             .text("We switched to distributing the plugin in the form of a specially prepared installation " +
             "package instead of offering the installation from sources. " +
             "The URL for fetching updates has changed because of this. " +
-            'However, there is no cause for concern: please go to "Software update" section of the OctoPrint ' +
-            "settings and update OctoRelay one more time. Sorry for the inconvenience. " +
+            'However, there is no cause for concern: please proceed to the "Software update" section of the ' +
+            "OctoPrint settings and update OctoRelay one more time. Sorry for the inconvenience. " +
             "As a last resort, you can always install it manually using the following URL: " +
             "https://github.com/borisbu/OctoRelay/releases/download/latest/release.zip");
         dialog
