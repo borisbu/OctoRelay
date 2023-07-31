565afc966fef37c559e063e8d12dbdf40b8f26fc
Robin Tail <robin_tail@me.com>
2023-08-01 01:45:31 +0200

    Revert "Checking the test failures in case of mutability."
    This reverts commit 3080cb01b54896b76719fbc0fe46f3831e6710a9.

--- octoprint_octorelay/const.py
+++ octoprint_octorelay/const.py
@@ -138,13 +138,11 @@ def get_default_settings():
 # Keys of the default settings, used for iterations: [r1...r8]
 RELAY_INDEXES = get_default_settings().keys()
 
-TEMPLATES = [
-    { "type": "navbar", "custom_bindings": False },
-    { "type": "settings", "custom_bindings": False }
-]
-
 def get_templates():
-    return TEMPLATES.copy()
+    return [
+        { "type": "navbar", "custom_bindings": False },
+        { "type": "settings", "custom_bindings": False }
+    ]
 
 # Plugin's asset files to automatically include in the core UI
 ASSETS = { "js": [ "js/octorelay.js" ] }
