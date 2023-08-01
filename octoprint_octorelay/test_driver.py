82335242e0d196853ec2970efa855ff291a5b659
Anna Bocharova <anna.bocharova@deliveryhero.com>
2023-08-01 11:37:47 +0200

    Draft test for the driver constructor.

---
+++ octoprint_octorelay/test_driver.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+import unittest
+import sys
+from unittest.mock import Mock, patch
+
+GPIO_mock = Mock()
+GPIO_mock.OUT = "MockedOUT"
+sys.modules["RPi.GPIO"] = GPIO_mock
+
+# pylint: disable=wrong-import-position
+from octoprint_octorelay.driver import Relay
+
+class TestRelayDriver(unittest.TestCase):
+    def test_constructor(self):
+        relay = Relay(18, False)
+        self.assertIsInstance(relay, Relay)
