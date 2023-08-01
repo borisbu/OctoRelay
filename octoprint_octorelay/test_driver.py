d404b749619919e508c28aa936ca048026c55282
Anna Bocharova <anna.bocharova@deliveryhero.com>
2023-08-01 13:09:16 +0200

    runtime patching of GPIO in driver test.

--- octoprint_octorelay/test_driver.py
+++ octoprint_octorelay/test_driver.py
@@ -1,30 +1,29 @@
 # -*- coding: utf-8 -*-
 import unittest
 import sys
-from unittest.mock import Mock
-
-GPIO_mock = Mock()
-GPIO_mock.OUT = "MockedOUT"
-sys.modules["RPi.GPIO"] = GPIO_mock
-
-print(GPIO_mock)
-
-# pylint: disable=wrong-import-position
-from octoprint_octorelay.driver import Relay
+from unittest.mock import Mock, patch
 
 class TestRelayDriver(unittest.TestCase):
-    def test_constructor(self):
+
+    @patch('RPi.GPIO')
+    def test_constructor(self, GPIO_mock):
+        from octoprint_octorelay.driver import Relay
         relay = Relay(18, True)
         self.assertIsInstance(relay, Relay)
         self.assertEqual(relay.pin, 18)
         self.assertTrue(relay.inverted)
 
-    def test_serialization(self):
+    @patch('RPi.GPIO')
+    def test_serialization(self, GPIO_mock):
+        from octoprint_octorelay.driver import Relay
         relay = Relay(18, True)
         serialization = f"{relay}"
         self.assertEqual(serialization, "Relay(pin=18,inverted=True)")
 
-    def test_close(self):
+    @patch('RPi.GPIO')
+    def test_close(self, GPIO_mock):
+        GPIO_mock.OUT = "MockedOUT"
+        from octoprint_octorelay.driver import Relay
         cases = [
             { "relay": Relay(18, False), "expected_pin_state": True },
             { "relay": Relay(18, True), "expected_pin_state": False }
@@ -36,7 +35,10 @@ class TestRelayDriver(unittest.TestCase):
             GPIO_mock.setwarnings.assert_any_call(False)
             GPIO_mock.setwarnings.assert_called_with(True)
 
-    def test_open(self):
+    @patch('RPi.GPIO')
+    def test_open(self, GPIO_mock):
+        GPIO_mock.OUT = "MockedOUT"
+        from octoprint_octorelay.driver import Relay
         cases = [
             { "relay": Relay(18, False), "expected_pin_state": False },
             { "relay": Relay(18, True), "expected_pin_state": True }
@@ -48,7 +50,9 @@ class TestRelayDriver(unittest.TestCase):
             GPIO_mock.setwarnings.assert_any_call(False)
             GPIO_mock.setwarnings.assert_called_with(True)
 
-    def test_get_pin_state(self):
+    @patch('RPi.GPIO')
+    def test_get_pin_state(self, GPIO_mock):
+        from octoprint_octorelay.driver import Relay
         cases = [
             { "mocked_state": 1, "inverted": False, "expected_pin_state": True },
             { "mocked_state": 0, "inverted": False, "expected_pin_state": False },
@@ -63,7 +67,9 @@ class TestRelayDriver(unittest.TestCase):
             GPIO_mock.setwarnings.assert_called_with(True)
             GPIO_mock.input.assert_called_with(18)
 
-    def test_is_closed(self):
+    @patch('RPi.GPIO')
+    def test_is_closed(self, GPIO_mock):
+        from octoprint_octorelay.driver import Relay
         cases = [
             { "mocked_state": 1, "inverted": False, "expected_relay_state": True },
             { "mocked_state": 0, "inverted": False, "expected_relay_state": False },
@@ -78,7 +84,10 @@ class TestRelayDriver(unittest.TestCase):
             GPIO_mock.setwarnings.assert_called_with(True)
             GPIO_mock.input.assert_called_with(18)
 
-    def test_toggle__no_argument(self):
+    @patch('RPi.GPIO')
+    def test_toggle__no_argument(self, GPIO_mock):
+        GPIO_mock.OUT = "MockedOUT"
+        from octoprint_octorelay.driver import Relay
         cases = [
             { "mocked_state": 1, "inverted": False, "expected_pin_state": False, "expected_relay_state": False },
             { "mocked_state": 0, "inverted": False, "expected_pin_state": True, "expected_relay_state": True },
