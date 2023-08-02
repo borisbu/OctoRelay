2048a2872784205ef00b15b14a96a5adf42254dd
Anna Bocharova <robin_tail@me.com>
2023-08-02 12:03:19 +0000

    Minor: typo.

--- octoprint_octorelay/driver.py
+++ octoprint_octorelay/driver.py
@@ -2,7 +2,7 @@
 from typing import Optional
 from RPi import GPIO
 
-# The driver operated BCM mode of pins enumeration
+# The driver operates BCM mode of pins enumeration
 GPIO.setmode(GPIO.BCM)
 
 def xor(left: bool, right: bool) -> bool:
