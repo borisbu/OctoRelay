209051c8d6a79fd55d3731df64fe514fd00ba751
Anna Bocharova <robin_tail@me.com>
2023-08-02 20:22:05 +0000

    Ref: DNRY: easier growing system for running migrations in the future.

--- octoprint_octorelay/migrations.py
+++ octoprint_octorelay/migrations.py
@@ -1,9 +1,13 @@
 # -*- coding: utf-8 -*-
 def migrate(current: int, settings, logger):
-    if current < 1:
-        migrate_to_v1(settings, logger)
+    # List of migration functions beginning from the one needed to migrate from v0 to v1
+    migrations = [ to_v1 ]
+    # Current version number corresponds to the list index to begin migrations from
+    jobs = migrations[current::]
+    for job in jobs:
+        job(settings, logger)
 
-def migrate_to_v1(settings, logger):
+def to_v1(settings, logger):
     # First 4 relays used to have active=True
     logger.info("OctoRelay migrates to settings v1")
     for index in ["r1", "r2", "r3", "r4"]:
