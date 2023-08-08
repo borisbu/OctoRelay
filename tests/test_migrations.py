5d706d18c771cd2b25c890e80864f4139a0d797e
Anna Bocharova <robin_tail@me.com>
2023-08-08 09:41:49 +0000

    Sample test for migration script.

--- tests/test_migrations.py
+++ tests/test_migrations.py
@@ -13,7 +13,7 @@ sys.modules["RPi.GPIO"] = Mock()
 
 # pylint: disable=wrong-import-position
 from octoprint_octorelay.const import SETTINGS_VERSION
-from octoprint_octorelay.migrations import migrators, migrate, v0, v1
+from octoprint_octorelay.migrations import migrators, migrate, v0, v1, v2
 
 # avoid keeping other modules automatically imported by this test
 del sys.modules["octoprint_octorelay"]
@@ -79,6 +79,59 @@ class TestMigrations(unittest.TestCase):
                 "auto_on_before_print": True,
                 "auto_off_after_print": True,
                 "auto_off_delay": 10,
+            })
+
+    def test_v2(self):
+        settings = Mock(
+            get=Mock(return_value={
+                "active": False,
+                "relay_pin": 23,
+                "inverted_output": True,
+                "initial_value": True,
+                "cmd_on": "sudo service webcamd start",
+                "cmd_off": "sudo service webcamd stop",
+                "icon_on": """<img width="24" height="24" src="/plugin/octorelay/static/img/webcam.svg" >""",
+                "icon_off": (
+                    """<img width="24" height="24" src="/plugin/octorelay/static/img/webcam.svg" """
+                    """style="filter: opacity(20%)">"""
+                ),
+                "label_text": "Webcam",
+                "confirm_off": False,
+                "auto_on_before_print": True,
+                "auto_off_after_print": True,
+                "auto_off_delay": 10,
+            })
+        )
+        logger = Mock()
+        v2(settings, logger)
+        for index in ["r1", "r2", "r3", "r4", "r5", "r6", "r7", "r8"]:
+            settings.set.assert_any_call([index], {
+                "active": False,
+                "relay_pin": 23,
+                "inverted_output": True,
+                "cmd_on": "sudo service webcamd start",
+                "cmd_off": "sudo service webcamd stop",
+                "icon_on": """<img width="24" height="24" src="/plugin/octorelay/static/img/webcam.svg" >""",
+                "icon_off": (
+                    """<img width="24" height="24" src="/plugin/octorelay/static/img/webcam.svg" """
+                    """style="filter: opacity(20%)">"""
+                ),
+                "label_text": "Webcam",
+                "confirm_off": False,
+                "rules": {
+                    "STARTUP": {
+                        "state": True,
+                        "delay": 0
+                    },
+                    "PRINTING_STARTED": {
+                        "state": True,
+                        "delay": 0
+                    },
+                    "PRINTING_STOPPED": {
+                        "state": False,
+                        "delay": 10
+                    }
+                }
             })
 
     def test_migrate(self):
