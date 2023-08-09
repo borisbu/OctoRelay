c92cd9b9426d6e9dc572414184930337381ad0ee
Robin Tail <robin_tail@me.com>
2023-08-09 21:16:21 +0200

    Adjusting the deployment script.

--- deployAndRestart.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-target="pi@octopi-dev.local";
-echo "deploy to octopi: ${target}"
-rsync -avzh --exclude='.git/' --exclude='.DS_Store' --exclude='deployAndRestart.sh' --delete . ${target}:~/OctoRelay
-ssh ${target} "~/OctoRelay/installOnPiAndRestart.sh"
