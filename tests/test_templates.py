0da5490784e016a8e1d600aca1dda90569854e76
Anna Bocharova <robin_tail@me.com>
2023-08-10 15:33:23 +0000

    Improving test environment for templates.

--- tests/test_templates.py
+++ tests/test_templates.py
@@ -1,8 +1,19 @@
 # -*- coding: utf-8 -*-
 import unittest
+import sys
+from unittest.mock import Mock
 from snapshottest import TestCase
 from jinja2 import Environment, FileSystemLoader
+
+# Mocking required before the further import
+sys.modules["RPi.GPIO"] = Mock()
+
+# pylint: disable=wrong-import-position
 from octoprint_octorelay.const import get_ui_vars
+
+# avoid keeping other modules automatically imported by this test
+if "octoprint_octorelay" in sys.modules:
+    del sys.modules["octoprint_octorelay"]
 
 environment = Environment(loader=FileSystemLoader("../octoprint_octorelay/templates/"))
 
