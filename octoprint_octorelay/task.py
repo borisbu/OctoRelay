c24741044a868e35e70abf750ada2a9f0850d96f
Anna Bocharova <robin_tail@me.com>
2023-08-12 11:42:01 +0000

    Add serializer for Task.

--- octoprint_octorelay/task.py
+++ octoprint_octorelay/task.py
@@ -10,3 +10,6 @@ class Task():
         self.deadline = time.time() + delay
         # https://github.com/OctoPrint/OctoPrint/blob/ed4a264/src/octoprint/util/__init__.py#L1319
         self.timer = ResettableTimer(delay, function, args)
+
+    def __repr__(self) -> str:
+        return f"{type(self).__name__}({self.subject},{self.target},{self.owner})"
