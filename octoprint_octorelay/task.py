b47c53f7003b0564d58b33c89ffd24532ff470e2
Anna Bocharova <robin_tail@me.com>
2023-08-12 13:26:55 +0000

    Add delay field to Task.

--- octoprint_octorelay/task.py
+++ octoprint_octorelay/task.py
@@ -7,9 +7,10 @@ class Task():
         self.subject = subject
         self.target = target
         self.owner = owner
+        self.delay = delay
         self.deadline = time.time() + delay
         # https://github.com/OctoPrint/OctoPrint/blob/ed4a264/src/octoprint/util/__init__.py#L1319
         self.timer = ResettableTimer(delay, function, args)
 
     def __repr__(self) -> str:
-        return f"{type(self).__name__}({self.subject},{self.target},{self.owner})"
+        return f"{type(self).__name__}({self.subject},{self.target},{self.owner},{self.delay})"
