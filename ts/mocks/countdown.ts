141b9c625f6bec67b661fd1885b1adc93f1e5b77
Robin Tail <robin_tail@me.com>
2023-09-30 00:25:55 +0200

    Fix: deadline mocking returns.

--- ts/mocks/countdown.ts
+++ ts/mocks/countdown.ts
@@ -1,6 +1,6 @@
 export const disposerMock = jest.fn();
 export const countdownMock = jest.fn(() => disposerMock);
-export const deadlineMock = jest.fn();
+export const deadlineMock = jest.fn(() => "sample deadline");
 
 jest.mock("../helpers/countdown", () => ({
   setCountdown: countdownMock,
