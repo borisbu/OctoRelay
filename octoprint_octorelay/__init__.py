464fc4b0987dcd9dd73830dc14fb118f05e7e548
Anna Bocharova <robin_tail@me.com>
2023-10-22 13:07:12 +0200

    Ref: Ensure int for version

--- octoprint_octorelay/__init__.py
+++ octoprint_octorelay/__init__.py
@@ -152,7 +152,7 @@ class OctoRelayPlugin(
     def on_api_command(self, command, data):
         # pylint: disable=too-many-return-statements
         self._logger.info(f"Received the API command {command} with parameters: {data}")
-        version = data.get("version") or data.get("v") or 1
+        version = int( data.get("version") or data.get("v") or 1 )
         subject_param_name = "pin" if version == 1 else "subject"
         subject = data.get(subject_param_name)
         target = data.get("target")
