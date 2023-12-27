a0744c21819cb4b740e70d9cf0849a86f64ba3f8
Robin Tail <robin_tail@me.com>
2023-12-27 23:55:59 +0100

    Switching to ESM dev environment.

--- ui/tsup.config.ts
+++ ui/tsup.config.ts
@@ -4,6 +4,7 @@ export default defineConfig({
   entry: ["octorelay.ts"],
   outDir: "../octoprint_octorelay/static/js",
   format: "cjs",
+  outExtension: () => ({ js: ".js" }),
   splitting: false,
   sourcemap: false,
   clean: true,
