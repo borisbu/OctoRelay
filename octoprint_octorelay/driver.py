f22dd17c9530c8169e3a83addada1f4e54150624
ch3p4ll3 <vidonimattia@gmail.com>
2024-06-29 17:08:24 +0200

    move to gpiozero

--- octoprint_octorelay/driver.py
+++ octoprint_octorelay/driver.py
@@ -1,9 +1,7 @@
 # -*- coding: utf-8 -*-
 from typing import Optional
-from RPi import GPIO
+from gpiozero import LED
 
-# The driver operates BCM mode of pins enumeration
-GPIO.setmode(GPIO.BCM)
 
 def xor(left: bool, right: bool) -> bool:
     return left is not right
@@ -12,25 +10,22 @@ class Relay():
     def __init__(self, pin: int, inverted: bool):
         self.pin = pin # GPIO pin
         self.inverted = inverted # marks the relay as normally closed
+        self.relay = LED(pin, active_high=inverted)
 
     def __repr__(self) -> str:
         return f"{type(self).__name__}(pin={self.pin},inverted={self.inverted},closed={self.is_closed()})"
 
     def close(self):
         """Activates the current flow through the relay."""
-        self.toggle(True)
+        self.relay.on()
 
     def open(self):
         """Deactivates the current flow through the relay."""
-        self.toggle(False)
+        self.relay.off()
 
     def is_closed(self) -> bool:
         """Returns the logical state of the relay."""
-        GPIO.setwarnings(False)
-        GPIO.setup(self.pin, GPIO.OUT)
-        pin_state = bool(GPIO.input(self.pin))
-        GPIO.setwarnings(True)
-        return xor(self.inverted, pin_state)
+        return self.relay.is_lit
 
     def toggle(self, desired_state: Optional[bool] = None) -> bool:
         """
@@ -38,10 +33,5 @@ class Relay():
         If the argument is not specified then switches based on the current state.
         Returns the new logical state of the relay.
         """
-        if desired_state is None:
-            desired_state = not self.is_closed()
-        GPIO.setwarnings(False)
-        GPIO.setup(self.pin, GPIO.OUT)
-        GPIO.output(self.pin, xor(self.inverted, desired_state))
-        GPIO.setwarnings(True)
+        self.relay.toggle()
         return desired_state
