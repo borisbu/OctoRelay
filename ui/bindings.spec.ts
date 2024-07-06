b4a73659636e909ef744f6668e35f8e2b88270a9
Robin Tail <robin_tail@me.com>
2024-07-06 13:59:59 +0200

    Replacing JSDOM with HappyDOM.

--- ui/bindings.spec.ts
+++ ui/bindings.spec.ts
@@ -1,13 +1,14 @@
 import { readFileSync } from "node:fs";
-import { JSDOM } from "jsdom";
+import { Window } from "happy-dom";
 import { describe, test, expect } from "vitest";
 
 describe("Knockout bindings", () => {
-  const html = readFileSync(
+  const window = new Window();
+  const document = window.document;
+  document.body.innerHTML = readFileSync(
     "../octoprint_octorelay/templates/octorelay_settings.jinja2",
     "utf-8",
   );
-  const document = JSDOM.fragment(html);
   const usingContexts = [
     "settings.plugins.octorelay.r{{n}}",
     "settings.plugins.octorelay",
