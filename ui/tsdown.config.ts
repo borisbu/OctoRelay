6e43b7a28c9d5e19d1606ae9c857ec6985f8db50
Anna Bocharova <robin_tail@me.com>
2025-12-15 06:09:42 +0100

    Switching to `tsdown`, `vite@8` and `rolldown` (#363)
    * Switching to tsdown.
    
    * Overriding vite to v8.0.0-beta.1.
    
    * Overriding lightningcss.
    
    * Changing format to ESM.

--- ui/tsdown.config.ts
+++ ui/tsdown.config.ts
@@ -1,12 +1,10 @@
-import { defineConfig } from "tsup";
+import { defineConfig } from "tsdown";
 
 export default defineConfig({
   entry: ["octorelay.ts"],
   outDir: "../octoprint_octorelay/static/js",
-  format: "cjs",
-  outExtension: () => ({ js: ".js" }),
   platform: "browser",
-  splitting: false,
+  target: "es6",
   sourcemap: false,
   clean: true,
   dts: false,
